2f89356fa645bc6753a103e8a8217d86d17bca1f
Gustavo Henrique Ribeiro Dias <gugahribeiro@hotmail.com>
2022-08-09 10:49:51 -0300

    feat: connect to redux

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,10 +1,40 @@
-const Dashboard = ():JSX.Element => {
+import {connect} from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
+import { ApplicationState } from '../store';
+import * as AuthenticationActions from '../store/ducks/authentication/actions';
+import { Authentication, LoginRequestPayload } from '../store/ducks/authentication/types';
+
+interface StateProps {
+	authentication: Authentication;
+	loading: boolean;
+	error: boolean;
+}
+
+interface DispatchProps {
+	loginRequest(data: LoginRequestPayload): void;
+	logoutRequest(): void;
+}
+
+interface OwnProps { }
+
+type DashboardProps = StateProps & DispatchProps & OwnProps;
+
+const Dashboard = ({authentication, logoutRequest}: DashboardProps):JSX.Element => {
 	return (
 		<div>
 			<h1>Dashboard</h1>
-			<button>Sair</button>
+			<h2>Olá, {authentication.user.name}</h2>
+			<button onClick={logoutRequest}>Sair</button>
 		</div>
 	)
 }
 
-export default Dashboard
+const mapStateToProps = ({authentication}:ApplicationState) => ({
+	authentication: authentication.data,
+	loading: authentication.loading,
+	error: authentication.error,
+})
+
+const mapDispatchToProps = (dispatch: Dispatch) =>
+	bindActionCreators(AuthenticationActions, dispatch);
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
